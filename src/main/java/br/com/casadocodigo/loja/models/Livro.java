3e76cc342c86b506749527fb5a8e7b6919cc72f8
Alexandre Gonçalves <alexandre.corretor22@hotmail.com>
2021-07-14 10:59:47 -0300

    Finalizando a aula 1

--- src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -2,6 +2,7 @@ package br.com.casadocodigo.loja.models;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -33,9 +36,12 @@ public class Livro {
 	private BigDecimal preco;
 	@Min(10)
 	private Integer numeroPaginas;
+    
+	@Temporal(TemporalType.DATE)
+	private Calendar dataPublicacao= Calendar.getInstance();
+	
 	@ManyToMany
 	@Size(min = 1)
-	
 	@NotNull
 	private List<Autor> autores = new ArrayList<>();
 
@@ -77,6 +83,16 @@ public class Livro {
 
 	public void setNumeroPaginas(Integer numeroPaginas) {
 		this.numeroPaginas = numeroPaginas;
+	}
+	
+	
+
+	public Calendar getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public void setDataPublicacao(Calendar dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
 	}
 
 	@Override
